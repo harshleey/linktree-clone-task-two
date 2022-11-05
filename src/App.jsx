9bdcbdb34d5bd7567764b027b938b8a1ee2bb43d
Wonuola <logun.fateem@gmail.com>
2022-11-05 22:44:26 +0100

    fix: A bug fix

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <div className='App'>
       <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/contact" element={<Contact />} />
+          <Route path="contact" element={<Contact />} />
           <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </div>
