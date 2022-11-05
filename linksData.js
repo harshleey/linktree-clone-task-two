0524e1570888778e4aeb23f4a35076702cdd4ed1
Wonuola <logun.fateem@gmail.com>
2022-11-05 06:55:50 +0100

    feat: added a data file

---
+++ linksData.js
@@ -0,0 +1,38 @@
+export default [
+    {
+        id: 1,
+        idName: "twitter",
+        link: "twitter.com/harshleey",
+        webName: "Twitter Link"
+    },
+    {
+        id: 2,
+        idName: "button_zuri",
+        link: "training.zuri.team/",
+        webName: "Zuri Team"
+    },
+    {
+        id: 3,
+        idName: "",
+        link: "books.zuri.team/",
+        webName: "Zuri Books"
+    },
+    {
+        id: 4,
+        idName: "",
+        link: "books.zuri.team/python-for-beginners?ref_id=<yourslackname>",
+        webName: "Python Books"
+    },
+    {
+        id: 5,
+        idName: "",
+        link: "background.zuri.team/",
+        webName: "Background Check for Coders"
+    },
+    {
+        id: 6,
+        idName: "",
+        link: "books.zuri.team/design-rules",
+        webName: "Design Books"
+    }
+]
