d9707756bad6a9ab630e0cce145611df4811d7bb
Wonuola <logun.fateem@gmail.com>
2022-11-05 08:24:50 +0100

    Fix: A bug fix

--- linksData.js
+++ linksData.js
@@ -1,38 +1,42 @@
 export default [
     {
         id: 1,
-        idName: "twitter",
-        link: "twitter.com/harshleey",
-        webName: "Twitter Link"
+        idName: "",
+        link: "twitter.com/adebola_xo",
+        webName: "Twitter Link",
     },
     {
         id: 2,
-        idName: "button_zuri",
+        idName: "btn__zuri",
         link: "training.zuri.team/",
-        webName: "Zuri Team"
+        webName: "Zuri Team",
     },
     {
         id: 3,
-        idName: "",
+        idName: "books",
         link: "books.zuri.team/",
-        webName: "Zuri Books"
+        webName: "Zuri Books",
+        title:"Your one stop for getting books on design and coding"
     },
     {
         id: 4,
-        idName: "",
-        link: "books.zuri.team/python-for-beginners?ref_id=<yourslackname>",
-        webName: "Python Books"
+        idName: "book__pyhton",
+        link: "books.zuri.team/python-for-beginners?ref_id=wonuola",
+        webName: "Python Books",
+        title:"Get access to resources to help get an headstart on your tech career"
     },
     {
         id: 5,
-        idName: "",
+        idName: "pitch",
         link: "background.zuri.team/",
-        webName: "Background Check for Coders"
+        webName: "Background Check for Coders",
+        title:"Come give your code some validation at affordable prices"
     },
     {
         id: 6,
-        idName: "",
+        idName: "book__design",
         link: "books.zuri.team/design-rules",
-        webName: "Design Books"
+        webName: "Design Books",
+        title:"Want to be a designer? Start Here"
     }
 ]
